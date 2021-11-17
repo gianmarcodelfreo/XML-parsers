75d96cb986cbc2eb5769a3ae8e371828101def51
Mattia Simoncini <mattia.simoncini@davincifascetti.it>
2021-11-17 17:41:49 +0100

    Update SAX Parser

--- SAX_Parser/src/DefaultHandler_example.java
+++ SAX_Parser/src/DefaultHandler_example.java
@@ -1,81 +1,97 @@
 import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class DefaultHandler_example extends DefaultHandler {
+	private String tabber = "";
+    private StringBuilder currentValue = new StringBuilder();
 
-  private StringBuilder currentValue = new StringBuilder();
+    // Metodo che viene richiamato all'inizio del documento  
+    @Override
+    public void startDocument() {
+        System.out.println("Inizio del documento");
+    }
 
-  // Metodo che viene richiamato all'inizio del documento  
-  @Override
-  public void startDocument() {
-      System.out.println("Inizio del documento");
-  }
-  
-  // Metodo che viene richiamato alla fine del documento  
-  @Override
-  public void endDocument() {
-      System.out.println("Fine del documento");
-  }
+    // Metodo che viene richiamato alla fine del documento  
+    @Override
+    public void endDocument() {
+        System.out.println("Fine del documento");
+    }
 
-  // Metodo che viene richiamato all'inizio di un elemento
-  @Override
-  public void startElement(
-          String uri,
-          String localName,
-          String qName,
-          Attributes attributes) {
+    // Metodo che viene richiamato all'inizio di un elemento
+    @Override
+    public void startElement(
+        String uri,
+        String localName,
+        String qName,
+        Attributes attributes) {
 
-	  //Utile ad azzerare il valore corrente
-	  currentValue.setLength(0);
+        //Utile ad azzerare il valore corrente
+        currentValue.setLength(0);
 
-      System.out.println("Inizio dell'elemento : " + qName);
+        System.out.println("\n" + tabber + "Inizio dell'elemento : " + qName);
 
-      if (qName.equalsIgnoreCase("section")) {
-          String section = attributes.getValue("section");
-          System.out.println("Sezione : " + section);
-      }
+        if (qName.equalsIgnoreCase("section")) {
+            String section = attributes.getValue("section");
+            System.out.println(tabber + "Sezione : " + section);
+        }
 
-      if (qName.equalsIgnoreCase("specialization")) {
-          String specialization = attributes.getValue("specialization");
-          System.out.println("Specializzazione : " + specialization);
-      }
+        if (qName.equalsIgnoreCase("specialization")) {
+            String specialization = attributes.getValue("specialization");
+            System.out.println(tabber + "Specializzazione : " + specialization);
+        }
+        this.tabber += "\t";
+    }
 
-  }
+    // Metodo che viene richiamato alla fine di un elemento
+    @Override
+    public void endElement(String uri,
+        String localName,
+        String qName) {
+    	
+        if (qName.equalsIgnoreCase("name")) {
+            System.out.println(tabber + "name : " + currentValue.toString());
+        }
 
-  // Metodo che viene richiamato alla fine di un elemento
-  @Override
-  public void endElement(String uri,
-                         String localName,
-                         String qName) {
+        if (qName.equalsIgnoreCase("surname")) {
+            System.out.println(tabber + "surname : " + currentValue.toString());
+        }
 
-      System.out.println("Fine dell'elemento : " + qName);
+        if (qName.equalsIgnoreCase("age")) {
+            System.out.println(tabber + "age : " + currentValue.toString());
+        }
 
-      if (qName.equalsIgnoreCase("name")) {
-          System.out.println("name : " + currentValue.toString());
-      }
+        if (qName.equalsIgnoreCase("gender")) {
+            System.out.println(tabber + "gender : " + currentValue.toString());
+        }
 
-      if (qName.equalsIgnoreCase("surname")) {
-          System.out.println("surname : " + currentValue.toString());
-      }
+        if (qName.equalsIgnoreCase("dataOfBirth")) {
+            System.out.println(tabber + "dataOfBirth : " + currentValue.toString());
+        }
+        
+        this.tabber = this.tabber.replaceFirst("\t", "");
+        
+        System.out.println(tabber + "Fine dell'elemento : " + qName + "\n");
+    }
 
-      if (qName.equalsIgnoreCase("age")) {
-          System.out.println("age : " + currentValue.toString());
-      }
+    // Metodo che viene richiamato ogni volta che viene incontrato un carattere di testo
+    @Override
+    public void characters(char ch[], int start, int length) {
+        currentValue.append(ch, start, length);
+    }
 
-      if (qName.equalsIgnoreCase("gender")) {
-          System.out.println("gender : " + currentValue.toString());
-      }
-      
-      if (qName.equalsIgnoreCase("dataOfBirth")) {
-          System.out.println("dataOfBirth : " + currentValue.toString());
-      }
+    //Riceve la notifica di uno spazio ingorabile
+    @Override
+    public void ignorableWhitespace(char ch[], int start, int length)
+    throws SAXException {
+        System.out.println(tabber + "Spazio ignorabile");
+    }
 
-  }
-
-  // Metodo che viene richiamato ogni volta che viene incontrato un carattere di testo
-  @Override
-  public void characters(char ch[], int start, int length) {
-      currentValue.append(ch, start, length);
-  }
+    //Riceve la notifica di una PI.
+    @Override
+    public void processingInstruction(String target, String data)
+    throws SAXException {
+        System.out.println(tabber + "Istruzione di processamento (Target,Data): " + target + "," + data);
+    }
 
 }
--- SAX_Parser/src/SAXParser_example.java
+++ SAX_Parser/src/SAXParser_example.java
@@ -7,17 +7,27 @@ import java.io.IOException;
 
 public class SAXParser_example {
 
-    private static final String FILEXML = "input/input.xml";
+	//Percorso del file XML
+    private static final String FILEXML = "src/input/input.xml";
 
     public static void main(String[] args) {
 
+    	//Si crea un'istanza di SAXParserFactory
         SAXParserFactory factory = SAXParserFactory.newInstance();
+
         try {
+        	//Si crea un'istanza di SAXParserFactory
             SAXParser saxParser = factory.newSAXParser();
+            
+            //Si crea un oggetto di tipo DefaultHandler_example
             DefaultHandler_example handler = new DefaultHandler_example();
+            
+            //Si indica il file da parsare e l'handler
             saxParser.parse(FILEXML, handler);
+            
+            //Errori che si possono verificare
         } catch (SAXException | ParserConfigurationException | IOException e) {
-           	System.out.println("⚠️ E' avvenuto un errore durante il parsing ⚠️");
+            System.out.println("⚠️ E' avvenuto un errore durante il parsing ⚠️");
         }
 
     }
