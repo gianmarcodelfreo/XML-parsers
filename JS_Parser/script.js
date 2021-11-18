87f8e1df031069ef720522360cc72fae082e49ca
Gian Marco Del Freo <39838172+Gian-Marco-Del-Freo@users.noreply.github.com>
2021-11-18 15:16:54 +0100

    js parser update

--- JS_Parser/script.js
+++ JS_Parser/script.js
@@ -3,23 +3,20 @@ if (window.XMLHttpRequest) {
 }
 
 xmlhttp.onload = () => {
-  const xmlInput = new DOMParser().parseFromString(
-    xmlhttp.responseText,
-    "text/xml"
-  );
+  const xmlInput = new DOMParser().parseFromString(xmlhttp.responseText, "text/xml");
 
   const list = document.getElementById("content");
 
   const student = xmlInput.getElementsByTagName("student");
 
+  // Recupero dei dati
   for (let i = 0; i < student.length; i++) {
     const studentInfo = {
       name: student[i].getElementsByTagName("name")[0].textContent,
       surname: student[i].getElementsByTagName("surname")[0].textContent,
       age: student[i].getElementsByTagName("age")[0].textContent,
       gender: student[i].getElementsByTagName("gender")[0].textContent,
-      dataOfBirth:
-        student[i].getElementsByTagName("dataOfBirth")[0].textContent,
+      dataOfBirth: student[i].getElementsByTagName("dataOfBirth")[0].textContent,
     };
 
     list.innerHTML += `
@@ -32,6 +29,18 @@ xmlhttp.onload = () => {
       </ul>
     `;
   }
+
+  // Agire sul contenuto
+  const firstNameTag = xmlInput.getElementsByTagName("name")[0];
+  console.log("Nome: ", firstNameTag.textContent);
+  firstNameTag.innerHTML = "Linda";
+  console.log("Nome: ", firstNameTag.textContent);
+
+  // Agire sugli attributi
+  const classTag = xmlInput.getElementsByTagName("class")[0];
+  console.log("Attributo: ", classTag.getAttribute("specialization"));
+  classTag.setAttribute("specialization", "Elettrotecnica");
+  console.log("Attributo: ", classTag.getAttribute("specialization"));
 };
 
 xmlhttp.open("GET", "input.xml", false);
