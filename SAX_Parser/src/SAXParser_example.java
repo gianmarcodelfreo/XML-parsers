65a05a3b8b6a6d06daee85e83b2dd27132bda385
Tommaso-Parenti <65952237+Tommaso-Parenti@users.noreply.github.com>
2021-12-05 22:31:11 +0100

    SAX_Parser folder

---
+++ SAX_Parser/src/SAXParser_example.java
@@ -0,0 +1,55 @@
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class SAXParser_example {
+
+    static final String JAXP_SCHEMA_LANGUAGE =
+        "http://java.sun.com/xml/jaxp/properties/schemaLanguage";
+
+    static final String W3C_XML_SCHEMA =
+        "http://www.w3.org/2001/XMLSchema";
+
+    static final String JAXP_SCHEMA_SOURCE =
+        "http://java.sun.com/xml/jaxp/properties/schemaSource";
+
+    public static void main(String[] args) {
+        try {
+            //Validazione con XSD
+            URL urlXSD = new URL("https://raw.githubusercontent.com/Gian-Marco-Del-Freo/XML-parsers/main/inputs/template.xsd");
+            URL urlXML = new URL("https://raw.githubusercontent.com/Gian-Marco-Del-Freo/XML-parsers/main/inputs/example.xml");
+            Source xmlFile = new StreamSource(urlXML.openStream());
+            SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = schemaFactory.newSchema(urlXSD);
+            Validator validator = schema.newValidator();
+            validator.validate(xmlFile);
+            System.out.println("✅ Documento valido ✅ \n");
+
+            //Creazione SaxParser
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            factory.setNamespaceAware(true);
+            factory.setValidating(true);
+            SAXParser saxParser = factory.newSAXParser();
+            saxParser.setProperty(JAXP_SCHEMA_LANGUAGE, W3C_XML_SCHEMA);
+            saxParser.setProperty(JAXP_SCHEMA_SOURCE, "https://raw.githubusercontent.com/Gian-Marco-Del-Freo/XML-parsers/main/inputs/template.xsd");
+            DefaultHandler_example handler = new DefaultHandler_example();
+            saxParser.parse(urlXML.openStream(), handler);
+        } catch (SAXException | IOException | ParserConfigurationException e) {
+            System.out.println("⚠️ E' avvenuto un errore durante il parsing ⚠️: " + e);
+        }
+
+
+    }
+
+}
